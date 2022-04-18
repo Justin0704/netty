16daa90ca1f4c38fbb25ff7f26010c3b9c71acdd
Justin0704 <djlijian@126.com>
2022-04-19 00:30:24 +0800

    自定义换行符 - 解决粘包半包问题

--- basic/src/main/java/cn/enjoyedu/ch02/splicing/delimiter/DelimiterEchoClient.java
+++ basic/src/main/java/cn/enjoyedu/ch02/splicing/delimiter/DelimiterEchoClient.java
@@ -1,4 +1,21 @@
 package cn.enjoyedu.ch02.splicing.delimiter;
 
 public class DelimiterEchoClient {
+
+    private final String host;
+    private final int port;
+
+    public DelimiterEchoClient(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+
+    private void start(){
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
