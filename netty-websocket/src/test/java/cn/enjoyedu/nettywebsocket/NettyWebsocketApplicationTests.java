614ed960b4a216e229c9315e1d6f6c7fefec3530
Justin0704 <djlijian@126.com>
2022-04-22 01:41:40 +0800

    netty-websocket - 简单的群聊和单聊

---
+++ netty-websocket/src/test/java/cn/enjoyedu/nettywebsocket/NettyWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.enjoyedu.nettywebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
